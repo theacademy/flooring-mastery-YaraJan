dbf69287edbfa0dcb6751caa53573d4edd3682c1
YaraJan <yajandal@gmail.com>
2025-03-07 15:26:33 -0600

    Update OrderServiceLayerImplTest.java

--- FlooringMastery/src/test/java/com/yj/flooringmastery/service/OrderServiceLayerImplTest.java
+++ FlooringMastery/src/test/java/com/yj/flooringmastery/service/OrderServiceLayerImplTest.java
@@ -18,9 +18,7 @@ import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class OrderServiceLayerImplTest {
 
